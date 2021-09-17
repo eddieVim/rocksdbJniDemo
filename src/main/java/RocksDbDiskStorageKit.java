6aff419780868186ca40eba5508f82b012607040
eddieVim <eddievim@foxmail.com>
2021-09-17 13:22:02 +0800

    update

--- src/main/java/RocksDbDiskStorageKit.java
+++ src/main/java/RocksDbDiskStorageKit.java
@@ -2,7 +2,7 @@
  * @author eddie
  * @create 2021/9/17 13:18
  */
-public class RocksDbDiskStoreKit {
+public class RocksDbDiskStorageKit {
     public static void main(String[] args) {
         System.out.println("hello, world!");
     }
