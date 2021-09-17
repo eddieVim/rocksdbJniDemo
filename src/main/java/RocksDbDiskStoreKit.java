db8e7e1906ac2201f5bee4feb91ffbaccfc7d53e
eddieVim <eddievim@foxmail.com>
2021-09-17 13:20:43 +0800

    first commit

---
+++ src/main/java/RocksDbDiskStoreKit.java
@@ -0,0 +1,9 @@
+/**
+ * @author eddie
+ * @create 2021/9/17 13:18
+ */
+public class RocksDbDiskStoreKit {
+    public static void main(String[] args) {
+        System.out.println("hello, world!");
+    }
+}
